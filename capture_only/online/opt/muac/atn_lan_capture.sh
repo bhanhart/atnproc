9c77943123e8c45f43bcbb50ef11f20c0fc055e6
Bart Jan Hanhart <hsd@planet.nl>
2026-01-16 10:12:51 +0100

    Add files for online systemd ATN capture service

---
+++ capture_only/online/opt/muac/atn_lan_capture.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+
+: "${NETWORK_INTERFACE:=net3}"
+: "${ARCHIVE_DIR:=/archives/captures}"
+
+declare STOP_REQUESTED=0
+
+function signal_handler
+{
+    echo "Caught termination signal, stopping capture..."
+    STOP_REQUESTED=1
+}
+trap signal_handler SIGTERM SIGINT
+
+function _format_message
+{
+    local -r category="$1" ; shift
+    local -r message="$*"
+    printf "%s - %b" "${category}" "${message}"
+}
+
+function _to_stdout
+{
+    printf "%b\n" "$*"
+}
+
+function _to_stderr
+{
+    _to_stdout "$*" 1>&2
+}
+
+function info
+{
+    _to_stdout "$( _format_message "INFO " "$*" )"
+}
+
+function error
+{
+    _to_stderr "$( _format_message "ERROR" "$*" )"
+}
+
+function fatal
+{
+    _to_stderr "$( _format_message "FATAL" "$*" )"
+    exit 1
+}
+
+function is_process_running
+{
+    local -r pid="$1"
+    kill -0 "${pid}" 2>/dev/null
+}
+
+function stop_process
+{
+    local -r pid="$1"
+
+    if is_process_running "${pid}"
+    then
+        info "Sending SIGTERM to process ${pid}"
+        kill -TERM "${pid}"
+        sleep 2
+        if is_process_running "${pid}"
+        then
+            error "LAN capture process failed to stop"
+            info "Sending SIGKILL to process ${pid}"
+            kill -KILL "${pid}"
+        fi
+    fi
+}
+
+function main
+{
+    if ! command -v dumpcap >/dev/null 2>&1
+    then
+        fatal "No 'dumpcap' command found"
+    fi
+
+    mkdir -p "${ARCHIVE_DIR}"
+
+    local -r hostname="$(hostname -s)"
+    local -r capture_file="${ARCHIVE_DIR}"/"${hostname%%-*}"_"${NETWORK_INTERFACE}".pcap
+
+    info "Start capturing on interface ${NETWORK_INTERFACE} to file ${capture_file}"
+
+    # Use ring buffer with 72 files of 60 minutes each (total=3 days)
+    if ! dumpcap \
+-i "${NETWORK_INTERFACE}" \
+-p \
+-f 'not stp and not ether multicast and not ip multicast' \
+-b files:72 \
+-b duration:3600 \
+-P \
+-q \
+-w "${capture_file}" &
+    then
+        fatal "Failed to start LAN capture on interface ${NETWORK_INTERFACE}"
+    fi
+
+    local -r dumpcap_pid=$!
+    sleep 2
+    if ! is_process_running "${dumpcap_pid}"
+    then
+        fatal "LAN capture on interface ${NETWORK_INTERFACE} has failed"
+    fi
+
+    while (( STOP_REQUESTED == 0 ))
+    do
+        sleep 1
+        if ! is_process_running "${dumpcap_pid}"
+        then
+            fatal "LAN capture on interface ${NETWORK_INTERFACE} has failed"
+        fi
+    done
+    info "Stopped LAN capture on ${NETWORK_INTERFACE}, stopping capture process ..."
+    stop_process "${dumpcap_pid}"
+    info "Finished"
+}
+
+main
---
+++ capture_only/online/opt/muac/atn_lan_capture_cleanup.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+
+: "${ARCHIVE_DIR:=/archives/captures}"
+: "${RETENTION_DAYS:=7}"
+
+function _format_message
+{
+    local -r category="$1" ; shift
+    local -r message="$*"
+    printf "%s - %b" "${category}" "${message}"
+}
+
+function _to_stdout
+{
+    printf "%b\n" "$*"
+}
+
+function _to_stderr
+{
+    _to_stdout "$*" 1>&2
+}
+
+function info
+{
+    _to_stdout "$( _format_message "INFO " "$*" )"
+}
+
+function error
+{
+    _to_stderr "$( _format_message "ERROR" "$*" )"
+}
+
+function fatal
+{
+    _to_stderr "$( _format_message "FATAL" "$*" )"
+    exit 1
+}
+
+function main
+{
+    if ! command -v lsof >/dev/null 2>&1
+    then
+        fatal "No 'lsof' command found"
+    fi
+
+    if [[ -z "${ARCHIVE_DIR}" ]]
+    then
+        fatal "Variable 'ARCHIVE_DIR' not set"
+    fi
+
+    if [[ ! -d "${ARCHIVE_DIR}" ]]
+    then
+        info "Directory '${ARCHIVE_DIR}' does not exist"
+        return 0
+    fi
+
+    info "Searching '${ARCHIVE_DIR}' for files older than ${RETENTION_DAYS} days..."
+
+    # Files are separated by a NUL character due to the "-print0" option of the
+    # find command. Therefore, also set the separator to NUL using the -d ''
+    # option of the read command
+    while IFS= read -r -d '' file; do
+        # Just to be on the safe side, check whether the file is open
+        if lsof -- "$file" >/dev/null 2>&1
+        then
+            info "Skipping file '${file}' because it is currently open"
+            continue
+        fi
+
+        info "Deleting '${file}'"
+        rm -f -- "${file}"
+
+    done < <( find "${ARCHIVE_DIR}" -type f -mtime +"${RETENTION_DAYS}" -print0 )
+
+    info "Finished searching '${ARCHIVE_DIR}' for files older than ${RETENTION_DAYS} days"
+}
+
+main
